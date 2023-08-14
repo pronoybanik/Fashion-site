ece0fc4ebda6b6d19667498127c8aa32477374e4
PRONOY <pronoybanik82@gmail.com>
2023-08-14 20:59:09 +0600

    change navbar and header

--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -1,84 +1,90 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { Link } from 'react-router-dom'
-import img1 from '../Image/pexels-rachel-claire-5490969.jpg'
-import img2 from '../Image/pexels-mart-production-7679720.jpg'
-
+import { Link } from "react-router-dom";
+import img1 from "../Image/pexels-rachel-claire-5490969.jpg";
 
 const navigation = [
-  { name: 'Home', to: '/' },
-  { name: 'Category', to: '/category' },
-  { name: 'About', to: '/about' }
-]
+  { name: "Home", to: "/" },
+  { name: "Category", to: "/category" },
+  { name: "About", to: "/about" },
+  { name: "service", to: "/about" },
+];
 
 export default function Example() {
-  
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-
   return (
-    <div className="bg-white h-[800px]" style={{ backgroundImage: `url(${img1})`, backgroundSize: 'cover' }}
+    <div
+      className="bg-white h-[800px]"
+      style={{ backgroundImage: `url(${img1})`, backgroundSize: "cover" }}
     >
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <p className='text-[#f76803] font-bold text-lg font-serif uppercase'>Goenka's fashion</p>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12" >
-            {navigation.map((item) => (
-              <Link key={item.name} to={item.to} className="inline-block rounded border border-current px-8 py-3 text-sm font-bold  text-[#000000] transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-indigo-500">
-                {item.name}
-              </Link>
-            ))}
-          </div>
+      <div className="absolute inset-0 bg-black/40 h-[800px] sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
 
-        </nav>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-          <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <div key={item.name} to={item.to} className="text-sm mouse font-semibold leading-6  text-black">
-                      {item.name}
-                    </div>
-                  ))}
-                </div>
-
+      <header className="bg-white sticky top-0 z-50 ">
+        <div className="mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="md:flex md:items-center md:gap-12">
+              <div className="flex lg:flex-1">
+                <p className="text-[#f76803] font-bold text-lg font-serif uppercase">
+                  Goenka' s fashion
+                </p>
               </div>
             </div>
-          </Dialog.Panel>
-        </Dialog>
+
+            <div className="hidden md:block">
+              <nav aria-label="Global">
+                <ul className="flex items-center gap-6 text-sm">
+                  <li>
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        to={item.to}
+                        className="text-lg font-semibold mx-4 font-sans"
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <div className="sm:flex sm:gap-4">
+                <a
+                  className="rounded-md bg-[#2F4F4F] px-5 py-2.5 text-sm font-medium text-white shadow"
+                  href="/"
+                >
+                  Login
+                </a>
+
+                <div className="hidden sm:flex">
+                  <a
+                    className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                    href="/"
+                  >
+                    Register
+                  </a>
+                </div>
+              </div>
+
+              <div className="block md:hidden">
+                <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
@@ -90,21 +96,20 @@ export default function Example() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-
           <div className="text-center">
-            {/* <h1 className="text-4xl font-bold tracking-tight 4 sm:text-6xl">
+            <h1 className="text-4xl font-bold text-white tracking-tight 4 sm:text-6xl">
               Data to enrich your online business
-            </h1> */}
-            {/* <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
-            </p> */}
-
+            </h1>
+            <p className="mt-6 text-xl leading-8 text-gray-100">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua.
+            </p>
           </div>
         </div>
         <div
@@ -115,11 +120,11 @@ export default function Example() {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/component/Title/Title.jsx
+++ src/component/Title/Title.jsx
@@ -1,92 +1,78 @@
-
 const Title = () => {
-    return (
-        <section className="max-w-screen-lg mx-auto lg:-mt-12 -mt-6 mb-10">
-        <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
-
-            <div className="col-span-2 ">
-                <h1 className="lg:text-8xl text-5xl font-bold font-serif ">wear the  </h1>
-                <div className="flex gap-2">
-                    <h2 className="lg:text-8xl text-5xl font-bold font-serif ">best.</h2>
-                    <h3 className="lg:w-42 lg:text-sm text-xs text-justify mr-2 lg:mt-1  ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, enim nulla impedit laudantium sapiente voluptatum facilis repudiandae cupiditate voluptatibus velit, officia assumenda optio! Minus voluptates rem recusandae? Nisi expedita, amet voluptate dolor in nam, voluptates, ab ullam similique facilis adipisci!</h3>
-                </div>
-            </div>
-
-            <div className="ml-4">
-
-
-                <div
-                    className="relative flex h-32 bg-[#2F4F4F] mb-1 items-start justify-between    p-4 shadow-xl sm:p-6 lg:p-8"
-
-                >
-                    <div className="flex items-center  pt-4 text-gray-500">
-
-
-                        <h3 className=" text-lg font-serif uppercase font-semibold text-white sm:text-xl">
-                            shop now
-                        </h3>
-                        <div>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-7 w-7 ml-20 text-white"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
-                                />
-                            </svg>
-                        </div>
-
-
-                    </div>
-
-
-                </div>
-
-                <div
-                    className="relative flex h-32 bg-[#000000] mb-1 items-start justify-between   p-4 shadow-xl  lg:p-8"
-
-                >
-                    <div className="flex items-center   pt-4 text-gray-500">
-
-
-                        <h3 className=" text-lg font-serif uppercase font-semibold text-white ">
-                            see promo
-                        </h3>
-                        <div>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-7 w-7 ml-20 text-white"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
-                                />
-                            </svg>
-                        </div>
-
-
-                    </div>
-
-
-                </div>
-
-            </div>
-
+  return (
+    <section className="max-w-screen-lg mx-auto lg:-mt-12 -mt-6 mb-10">
+      <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
+        <div className="col-span-2 ">
+          <h1 className="lg:text-8xl text-5xl font-bold font-serif ">
+            wear the{" "}
+          </h1>
+          <div className="flex gap-2">
+            <h2 className="lg:text-8xl text-5xl font-bold font-serif ">
+              best.
+            </h2>
+            <h3 className="lg:w-42 lg:text-sm text-xs text-justify mr-2 lg:mt-1  ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+              enim nulla impedit laudantium sapiente voluptatum facilis
+              repudiandae cupiditate voluptatibus velit, officia assumenda
+              optio! Minus voluptates rem recusandae? Nisi expedita, amet
+              voluptate dolor in nam, voluptates, ab ullam similique facilis
+              adipisci!
+            </h3>
+          </div>
         </div>
 
+        <div className="ml-4">
+          <div className="relative flex h-32 bg-[#2F4F4F] mb-1 items-start justify-between    p-4 shadow-xl sm:p-6 lg:p-8">
+            <div className="flex items-center  pt-4 text-gray-500">
+              <h3 className=" text-lg font-serif uppercase font-semibold text-white sm:text-xl">
+                shop now
+              </h3>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 ml-20 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative flex h-32 bg-[#000000] mb-1 items-start justify-between   p-4 shadow-xl  lg:p-8">
+            <div className="flex items-center   pt-4 text-gray-500">
+              <h3 className=" text-lg font-serif uppercase font-semibold text-white ">
+                see promo
+              </h3>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 ml-20 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
-    );
+  );
 };
 
 export default Title;
